cc21d47d57a2d6375dbd01d0fe2fabdbb7479e91
lucas-amaral-06 <lba4@cin.ufpe.br>
2025-03-28 20:32:29 -0300

    Mudando configuracoes.py
    Adicionei uma variável que contém informações sobre as peças em suas casas iniciais do jogo.

--- configuracoes.py
+++ configuracoes.py
@@ -35,8 +35,22 @@ tam_fonte_jogar = 30
 
 #Sobre a tela do xadrez.
 
-#Matriz que vai armazenar um grupo de objetos da classe Casa, que serão as 64 casas do jogo. Começa vazia, pois o tabuleiro ainda não foi desenhado.
-matriz_casas = []
+#Informações sobre a configuração inicial do xadrez.
+tabuleiro_inicial = {
+    'torre_preta' : [(7, 0), (7, 7)],
+    'cavalo_preto' : [(7, 1), (7, 6)],
+    'bispo_preto' : [(7, 2), (7, 5)],
+    'rainha_preta' : [(7, 3)],
+    'rei_preto' : [(7, 4)],
+    'peao_preto' : [(6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7)],
+    
+    'torre_branca' : [(0, 0), (0, 7)],
+    'cavalo_branco' : [(0, 1), (0, 6)],
+    'bispo_branco' : [(0, 2), (0, 5)],
+    'rainha_branca' : [(0, 3)],
+    'rei_branco' : [(0, 4)],
+    'peao_branco' : [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7)]
+}
 
 #Casa de origem e de destino selecionadas. Ambas começam vazias. Ex.: Se o usuário clica na casa a7, e além disso ele havia clicado anteriormente na casa a1, onde há uma torre, a torre deve ser movida de a1 para a7.
 casa_origem = ()
