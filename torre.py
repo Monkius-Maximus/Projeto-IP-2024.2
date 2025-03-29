1f7e83c62c4be28cc9535471bdc06d46bdb13b12
lucas-amaral-06 <lba4@cin.ufpe.br>
2025-03-29 08:59:41 -0300

    Update torre.py
    Breve mudança na classe da torre, levando o método construtor para a classe de base das peças.

--- torre.py
+++ torre.py
@@ -3,22 +3,10 @@ import funcoes_do_jogo as fj
 from base_peça import Base
 
 class Torre(Base):
-
-    """Um método construtor que define a casa ocupada pela nova torre, assim como seu tipo (caso tenha que ser consultada por alguma função externa) e cor (se é branca ou preta). 
     
-        O formato do input deve ser assim:
+    tipo = 'torre'
 
-        cor = 'branca' ou 'preta'
-        casa = (0, 0); (0, 1), etc."""
-    def __init__(self, cor, casa):
-
-        self.tipo = 'torre'
-        self.cor = cor
-        self.casa = casa
-
-        #A imagem é criada na função main pelo método 'criar_imagem' da classe, de acordo com o icon já importado pelo png. Isso é feito para evitar múltiplas importações de pngs, deixando o aplicativo mais pesado.
-        self.imagem = ()
+    #Uma função que retorna os lances possíveis a serem feitos pela Torre, de acordo com as posições de todas as outras peças do tabuleiro. Esta função deve ser acionada quando essa torre for selecionada pelo usuário.
+    def definir_lances_torre(self, info_peças):
         
-        #Define a posição a ser desenhada no tabuleiro, conforme a casa ocupada pela peça. Isso é uma função geral e não um método desta classe, pois tal função poderá ser usada também pelas classes de Peão, Cavalo, Bispo, etc.
-        self.pos = self.descobrir_pos(casa)
-    
+        pass
