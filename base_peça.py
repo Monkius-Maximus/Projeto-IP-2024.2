e383a5afa5c2c19f1186db84cb621eb6c7830d7e
lucas-amaral-06 <lba4@cin.ufpe.br>
2025-03-29 09:03:44 -0300

    Update base_peça.py
    Alterando a classe de base da peça para suportar a implementação de um método construtor padrão.

--- base_peça.py
+++ base_peça.py
@@ -1,5 +1,22 @@
 #Função com métodos basilares para todos os tipos de peça do jogo.
-class Base:
+class Base: 
+
+    """Um método construtor que define a casa ocupada pela nova torre, assim como seu tipo (caso tenha que ser consultada por alguma função externa) e cor (se é branca ou preta). 
+
+    O formato do input deve ser assim:
+
+    cor = 'branca' ou 'preta'
+    casa = (0, 0); (0, 1), etc."""    
+    def __init__(self, cor, casa, tam_tabuleiro):
+
+        self.cor = cor
+        self.casa = casa
+
+        #A imagem é criada na função main pelo método 'criar_imagem' da classe, de acordo com o icon já importado pelo png. Isso é feito para evitar múltiplas importações de pngs, deixando o aplicativo mais pesado.
+        self.imagem = ()
+        
+        #Define a posição a ser desenhada no tabuleiro, conforme a casa ocupada pela peça. Isso é uma função geral e não um método desta classe, pois tal função poderá ser usada também pelas classes de Peão, Cavalo, Bispo, etc.
+        self.pos = self.descobrir_pos(casa, tam_tabuleiro)
 
     #Move a peça pelo tabuleiro.
     def mover_peça(self, nova_casa):
@@ -14,18 +31,20 @@ class Base:
         self.criar_imagem(dict_icons[peça_nome])
 
     #Função para descobrir a posição na qual uma peça deve ser desenhada, com base na casa que está ocupando.
-    def descobrir_pos(self, casa):
+    def descobrir_pos(self, casa, tam_tabuleiro):
 
         linha = casa[0]
         coluna = casa[1]
 
-        #Primeiro, obtém-se as posições x e y da casa em questão.
-        x_casa = coluna * 100
-        y_casa = 800 - (linha + 1) * 100
+        tam_casa = tam_tabuleiro / 8
 
-        #Depois, aumenta-se 50 na posição x e 50 na posição y, para obter a posição do centro da casa.
-        x_centro_casa = x_casa + 50
-        y_centro_casa = y_casa + 50
+        #Primeiro, obtém-se as posições x e y da casa em questão.
+        x_casa = coluna * tam_casa
+        y_casa = tam_tabuleiro - (linha + 1) * tam_casa
+
+        #Depois, aumenta-se metade do tamanho de uma casa na posição x e o mesmo em y, para obter a posição do centro da casa. 
+        x_centro_casa = x_casa + tam_casa / 2
+        y_centro_casa = y_casa + tam_casa / 2
 
         return (x_centro_casa, y_centro_casa)
 
