439b779db8c1a0576704418c1f49103dccd0d98d
lucas-amaral-06 <lba4@cin.ufpe.br>
2025-03-29 09:01:38 -0300

    Update configuracoes.py
    Mudando as configurações iniciais para suportar proporção, tamanho e posição dos objetos iniciais segundo a resolução do monitor que usa o jogo.

--- configuracoes.py
+++ configuracoes.py
@@ -2,7 +2,7 @@
 FPS = 60
 
 #Sobre a tela do jogo em geral.
-tam_tela = tam_tela_x, tam_tela_y = 1000, 800
+tam_tela = tam_tela_x, tam_tela_y = 1100, 800
 
 #Sobre a tela principal (Menu).
 
@@ -12,22 +12,29 @@ str_janela_menu = 'Chess CIn'
 #Sobre o título do Menu.
 str_fonte_menu = 'Ubuntu' #Uma string com o nome da fonte do título.
 tam_fonte_menu = 48
+tam_x_menu = 529
+tam_y_menu = 54
 str_menu = 'Bem-vindo ao Chess CIn!' #Nome do título que aparecerá como principal.
 cor_menu = (0, 0, 0) #Define a cor do título do menu como azul.
 
 #Sobre o botão de Jogar. Definindo tamanho, cor e posição do botão Jogar.
 tam_jogar = tam_jogar_x, tam_jogar_y = 300, 100 #Define o tamanho do botão. Ordem: largura e comprimento.
-jogar_x = (tam_tela_x) / 2 - (tam_jogar_x / 2) #Define posição x (horizontal) do botão Jogar referente ao menu.
-jogar_y = (tam_tela_y) / 2 - (tam_jogar_y / 2) #Define posição y (vertical) do botão Jogar referente ao menu.
 cor_jogar = (255, 0, 0) #Define que a cor do botão 'Jogar' será vermelha.
 
-#Sobre o texto do botão jogar. Definindo string, cor e 
+#Sobre o texto do botão jogar. Definindo string, cor e fonte.
 str_jogar = 'Jogar'
 cor_txt_jogar = (0, 0, 0) #Define que a cor do texto 'Jogar' no botão será preta.
 str_fonte_jogar = 'Ubuntu'
 tam_fonte_jogar = 30
+tam_x_txt_jogar = 78
+tam_y_txt_jogar = 34
 
 #Sobre a tela do xadrez.
+
+#Tamanho do tabuleiro em pixels. Dimensão x e y.
+tam_tabuleiro = 800
+tam_casa = 100 #Tamanho da casa de xadrez.
+tam_peça = 80 #Tamanho que cada peça vai ocupar nas casas.
 
 #Informações sobre a configuração inicial do xadrez.
 tabuleiro_inicial = {
