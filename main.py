65619b007d1071c6cd109028ea28ea4d1c3a7bc3
lucas-amaral-06 <lba4@cin.ufpe.br>
2025-03-29 08:58:22 -0300

    Update main.py
    Implementando mudanças no desenho do tabuleiro de xadrez.

--- main.py
+++ main.py
@@ -20,39 +20,46 @@ usr_jogando = True
 #Inicializa a tela do programa de acordo com as exigências do menu.
 tela = fj._init_tela()
 
+#Salva os tamanhos x e y reais da tela, após ajustada para atender às proporções do monitor em questão.
+tela_x, tela_y = tela.get_size()
+
 #Objeto para administrar o tempo do programa.
 relógio = pygame.time.Clock()
 
-#Criando o título de menu e o retângulo correspondente à superfície.
-txt_menu, txt_menu_rect = fj._init_tit_menu()
+#Criando o título de menu e o retângulo correspondente à superfície de acordo com o tamanho da tela.
+txt_menu, pos_txt_menu = fj._init_tit_menu(tela_x, tela_y)
 
 #Criando o botão de jogar (superfície).
 
 #Definindo tamanho, superfície, cor e posição do botão Jogar.
-jogar, jogar_rect = fj._init_jogar()
+jogar, pos_jogar = fj._init_jogar(tela_x, tela_y)
 
 #Definindo fonte, texto, cor e superfície do texto do botão Jogar. O botão de Jogar é passado como parâmetro para definir a posição do texto.
-txt_jogar, txt_jogar_rect = fj._init_txt_jogar(jogar)
+txt_jogar, pos_txt_jogar = fj._init_txt_jogar(jogar)
 
 #Colocando o texto 'Jogar' na superfície do botão Jogar.
-jogar.blit(txt_jogar, txt_jogar_rect) #Desenha o texto 'Jogar' no botão Jogar.
+jogar.blit(txt_jogar, pos_txt_jogar) #Desenha o texto 'Jogar' no botão Jogar.
 
 #Sobre a tela do xadrez.
 
-# Importando as peças do xadrez.
-torre_preta = fj.importar_peças('torre_preta.png')
-cavalo_preto = fj.importar_peças('cavalo_preto.png')
-bispo_preto = fj.importar_peças('bispo_preto.png')
-rainha_preta = fj.importar_peças('rainha_preta.png')
-rei_preto = fj.importar_peças('rei_preto.png')
-peao_preto = fj.importar_peças('peao_preto.png')
+#Importando o tabuleiro.
+tabuleiro = fj.importar_tabuleiro(tela_x)
+tam_tabuleiro = tabuleiro.get_size()[0]
 
-torre_branca = fj.importar_peças('torre_branca.png')
-cavalo_branco = fj.importar_peças('cavalo_branco.png')
-bispo_branco = fj.importar_peças('bispo_branco.png')
-rainha_branca = fj.importar_peças('rainha_branca.png')
-rei_branco = fj.importar_peças('rei_branco.png')
-peao_branco = fj.importar_peças('peao_branco.png')
+# Importando as peças do xadrez.
+torre_preta = fj.importar_peças('torre_preta.png', tabuleiro)
+cavalo_preto = fj.importar_peças('cavalo_preto.png', tabuleiro)
+bispo_preto = fj.importar_peças('bispo_preto.png', tabuleiro)
+rainha_preta = fj.importar_peças('rainha_preta.png', tabuleiro)
+rei_preto = fj.importar_peças('rei_preto.png', tabuleiro)
+peao_preto = fj.importar_peças('peao_preto.png', tabuleiro)
+
+torre_branca = fj.importar_peças('torre_branca.png', tabuleiro)
+cavalo_branco = fj.importar_peças('cavalo_branco.png', tabuleiro)
+bispo_branco = fj.importar_peças('bispo_branco.png', tabuleiro)
+rainha_branca = fj.importar_peças('rainha_branca.png', tabuleiro)
+rei_branco = fj.importar_peças('rei_branco.png', tabuleiro)
+peao_branco = fj.importar_peças('peao_branco.png', tabuleiro)
 
 #Criando uma associação entre o nome de cada peça e a surface desenhável dela.
 dict_icons = {
@@ -70,9 +77,6 @@ dict_icons = {
     'rei_branco' : rei_branco,
     'peao_branco' : peao_branco,
 }
-
-#Importando o tabuleiro.
-tabuleiro = pygame.image.load('imagens/tabuleiro.png')
 
 #Casa de origem e de destino selecionadas. Ambas começam vazias. Ex.: Se o usuário clica na casa a7, e além disso ele havia clicado anteriormente na casa a1, onde há uma torre, a torre deve ser movida de a1 para a7.
 casa_origem = ()
@@ -105,29 +109,26 @@ while usr_jogando:
         #Se a tela atual for o menu, lida-se com os eventos interessantes que o menu pode receber.
         elif tela_atual == 'menu':
 
-            #Eventos de menu são resolvidos e a tela atual é atualizada. Além disso, quando o usuário apertar em 'Jogar', as informações sobre as peças são adicionadas na variável info_peças.
-            tela_atual, info_peças = fj.eventos_menu(evento, jogar_rect, dict_icons)
+            #Eventos de menu são resolvidos e a tela atual é atualizada. Além disso, quando o usuário apertar em 'Jogar', as informações sobre as peças são adicionadas na variável info_peças. Para isso, é necessário o uso da biblioteca dict_icons, para associar cada peça ao seu png. O retângulo do botão de jogar é passado para saber se o usuário clicou no botão.
+            tela_atual, info_peças = fj.eventos_menu(evento, jogar.get_rect(topleft=pos_jogar), dict_icons, tam_tabuleiro)
 
         #Se a tela for do jogo de xadrez, lida-se com os eventos interessantes que a tela do xadrez pode receber.
         elif tela_atual == 'xadrez':
 
             #Eventos da tela do xadrez são resolvidos e a tela atual é atualizada. A casa de destino não é enviada como parâmetro, pois, ela sempre será vazia ao checar novos eventos.
-            tela_atual, casa_origem, info_peças, vez = fj.eventos_xadrez(evento, casa_origem, info_peças, vez)
+            tela_atual, casa_origem, info_peças, vez = fj.eventos_xadrez(tam_tabuleiro, evento, casa_origem, info_peças, vez)
 
     #Desenhando as coisas no aplicativo de acordo com a tela do menu.
     if tela_atual == 'menu':
 
         #Desenha-se o menu com o título principal e o botão de jogar.
-        fj.desenhar_menu(tela, jogar, txt_menu, txt_menu_rect)
+        fj.desenhar_menu(tela, jogar, pos_jogar, txt_menu, pos_txt_menu)
 
     #Desenhando as coisas no aplicativo de acordo com a tela do xadrez.
     elif tela_atual == 'xadrez':
 
-        #Desenha-se o tabuleiro.
-        tela.blit(tabuleiro, (0, 0))
-
-        #Desenham-se as peças em cima do tabuleiro.
-        fj.desenhar_peças(tela, info_peças)
+        #Desenha-se a tela do xadrez com as peças e todo o resto.
+        fj.desenhar_xadrez(tela, tabuleiro, info_peças)
 
     #Atualiza a tela desde as últimas alterações.
     pygame.display.update()
