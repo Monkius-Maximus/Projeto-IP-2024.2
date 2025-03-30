4c7c531156359afe656002099b7cf54831371435
lucas-amaral-06 <lba4@cin.ufpe.br>
2025-03-30 05:17:45 -0300

    Mudando main.py
    Implementando mudanças:
    
    - Sidebar (Contagem);
    - Movimento efetivo da Torre;
    - Captura de peças;
    - Verificar de quem é a vez, casa de origem, casa de destino, etc.

--- main.py
+++ main.py
@@ -46,20 +46,57 @@ jogar.blit(txt_jogar, pos_txt_jogar) #Desenha o texto 'Jogar' no botão Jogar.
 tabuleiro = fj.importar_tabuleiro(tela_x)
 tam_tabuleiro = tabuleiro.get_size()[0]
 
-# Importando as peças do xadrez.
-torre_preta = fj.importar_peças('torre_preta.png', tabuleiro)
-cavalo_preto = fj.importar_peças('cavalo_preto.png', tabuleiro)
-bispo_preto = fj.importar_peças('bispo_preto.png', tabuleiro)
-rainha_preta = fj.importar_peças('rainha_preta.png', tabuleiro)
-rei_preto = fj.importar_peças('rei_preto.png', tabuleiro)
-peao_preto = fj.importar_peças('peao_preto.png', tabuleiro)
+#Importando as peças do xadrez.
+torre_preta, torre_preta_pequena = fj.importar_peças('torre_preta.png', tabuleiro)
+cavalo_preto, cavalo_preto_pequeno = fj.importar_peças('cavalo_preto.png', tabuleiro)
+bispo_preto, bispo_preto_pequeno = fj.importar_peças('bispo_preto.png', tabuleiro)
+rainha_preta, rainha_preta_pequena = fj.importar_peças('rainha_preta.png', tabuleiro)
+rei_preto = fj.importar_peças('rei_preto.png', tabuleiro)[0]
+peão_preto, peão_preto_pequeno = fj.importar_peças('peao_preto.png', tabuleiro)
 
-torre_branca = fj.importar_peças('torre_branca.png', tabuleiro)
-cavalo_branco = fj.importar_peças('cavalo_branco.png', tabuleiro)
-bispo_branco = fj.importar_peças('bispo_branco.png', tabuleiro)
-rainha_branca = fj.importar_peças('rainha_branca.png', tabuleiro)
-rei_branco = fj.importar_peças('rei_branco.png', tabuleiro)
-peao_branco = fj.importar_peças('peao_branco.png', tabuleiro)
+torre_branca, torre_branca_pequena = fj.importar_peças('torre_branca.png', tabuleiro)
+cavalo_branco, cavalo_branco_pequeno = fj.importar_peças('cavalo_branco.png', tabuleiro)
+bispo_branco, bispo_branco_pequeno = fj.importar_peças('bispo_branco.png', tabuleiro)
+rainha_branca, rainha_branca_pequena = fj.importar_peças('rainha_branca.png', tabuleiro)
+rei_branco = fj.importar_peças('rei_branco.png', tabuleiro)[0]
+peão_branco, peão_branco_pequeno = fj.importar_peças('peao_branco.png', tabuleiro)
+
+#Sobre a sidebar (Contadores).
+
+#Inicializa a posição das peças brancas capturadas na sidebar e insere elas em uma lista, assim como os textos correspondentes às capturas. 
+brancas_capturadas = fj._init_brancas_capturadas(torre_branca_pequena,
+                                                 cavalo_branco_pequeno,
+                                                 bispo_branco_pequeno,
+                                                 rainha_branca_pequena,
+                                                 peão_branco_pequeno,
+                                                 tam_tabuleiro)
+
+#Inicializar os textos das brancas capturadas na sidebar, com base na posição das brancas capturadas.
+textos_brancas = fj._init_textos(brancas_capturadas)
+
+#Inicializa a posição das peças pretas capturadas na sidebar e insere elas em uma lista, assim como os textos correspondentes às capturas. 
+pretas_capturadas = fj._init_pretas_capturadas(torre_preta_pequena,
+                                               cavalo_preto_pequeno,
+                                               bispo_preto_pequeno,
+                                               rainha_preta_pequena,
+                                               peão_preto_pequeno,
+                                               tam_tabuleiro)
+
+#Inicializar os textos das brancas capturadas na sidebar, com base na posição das brancas capturadas.
+textos_pretas = fj._init_textos(pretas_capturadas)
+
+#Junta as informações numa estrutura de dados só, para simplificar.
+sidebar_contagem = {
+    'ícones' : {
+        'brancas' : brancas_capturadas,
+        'pretas' : pretas_capturadas
+    },
+
+    'textos' : {
+        'brancas' : textos_brancas,
+        'pretas' : textos_pretas
+    }
+}
 
 #Criando uma associação entre o nome de cada peça e a surface desenhável dela.
 dict_icons = {
@@ -68,22 +105,22 @@ dict_icons = {
     'bispo_preto' : bispo_preto,
     'rainha_preta' : rainha_preta,
     'rei_preto' : rei_preto,
-    'peao_preto' : peao_preto,
+    'peao_preto' : peão_preto,
 
     'torre_branca' : torre_branca,
     'cavalo_branco' : cavalo_branco,
     'bispo_branco' : bispo_branco,
     'rainha_branca' : rainha_branca,
     'rei_branco' : rei_branco,
-    'peao_branco' : peao_branco,
+    'peao_branco' : peão_branco,
 }
 
 #Casa de origem e de destino selecionadas. Ambas começam vazias. Ex.: Se o usuário clica na casa a7, e além disso ele havia clicado anteriormente na casa a1, onde há uma torre, a torre deve ser movida de a1 para a7.
 casa_origem = ()
 casa_destino = ()
 
-#Variável armazenando de quem é a vez no jogo. 0 -> pretas; 1 -> brancas.
-vez = 1
+#Variável armazenando de quem é a vez no jogo.
+vez = 'brancas'
 
 #Loop principal do jogo.
 while usr_jogando:
@@ -129,6 +166,9 @@ while usr_jogando:
 
         #Desenha-se a tela do xadrez com as peças e todo o resto.
         fj.desenhar_xadrez(tela, tabuleiro, info_peças)
+
+        #Desenha-se a sidebar do xadrez, no que se diz respeito aos contadoress.
+        fj.desenhar_sidebar_contagem(tela, sidebar_contagem)
 
     #Atualiza a tela desde as últimas alterações.
     pygame.display.update()
