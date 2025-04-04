142d5416234563bcc3a2dbae06aa7ce651107e8f
Flavitche <fves@cin.ufpe.br>
2025-04-03 23:55:56 -0300

    Add files via upload

--- bispo.py
+++ bispo.py
@@ -2,37 +2,41 @@ import pygame
 from base_peça import Base
 
 class Bispo(Base):
-    def __init__(self, cor, casa, tam_tabuleiro, info_peças):
-        super().__init__(cor, casa, tam_tabuleiro, info_peças)
-        self.tipo = "bispo"
-        self.cor_tabuleiro = self.definir_cor_tabuleiro(casa)  # Opcional, só visual
-
-    def definir_cor_tabuleiro(self, casa):
-        linha, coluna = casa
-        return "claro" if (linha + coluna) % 2 == 0 else "escuro"
-
+    
+    tipo = 'bispo'
+    
     def movimentos_possíveis(self, info_peças):
-        movimentos = []
-        direcoes = [(1, 1), (1, -1), (-1, 1), (-1, -1)]  # Direções diagonais
-
-        for dx, dy in direcoes:
-            nova_linha, nova_coluna = self.casa
-
-            while True:
-                nova_linha += dx
-                nova_coluna += dy
-
-                if not (0 <= nova_linha < 8 and 0 <= nova_coluna < 8):
-                    break  # Fora do tabuleiro
-
-                pos = (nova_linha, nova_coluna)
-                peça_no_caminho = info_peças.get(pos)
-
-                if peça_no_caminho:
-                    if peça_no_caminho.cor != self.cor:
-                        movimentos.append(pos)  # Pode capturar
-                    break  # Para sempre ao encontrar qualquer peça
+        
+        linha_atual, coluna_atual = self.casa
+        cor_bispo = self.cor
+        movimentos_possíveis = []
+        
+        # Junta todas as peças no tabuleiro
+        casas_ocupadas = {peça.casa: peça for grupo_cor, peças in info_peças.items() for peça in peças}
+        
+        # Direções diagonais (somente as do bispo)
+        direções = [
+            (1, 1), (-1, -1), (1, -1), (-1, 1)
+        ]
+        
+        for d_linha, d_coluna in direções:
+            esbarrou = False
+            nova_linha, nova_coluna = linha_atual, coluna_atual
+            
+            while not esbarrou:
+                nova_linha += d_linha
+                nova_coluna += d_coluna
+                
+                # Fora do tabuleiro
+                esbarrou_fim = not (0 <= nova_linha <= 7) or not (0 <= nova_coluna <= 7)
+                esbarrou_peça = (nova_linha, nova_coluna) in casas_ocupadas
+                
+                if esbarrou_fim or esbarrou_peça:
+                    esbarrou = True
+                    
+                    if esbarrou_peça and casas_ocupadas[(nova_linha, nova_coluna)].cor != cor_bispo:
+                        movimentos_possíveis.append((nova_linha, nova_coluna))  # Captura possível
                 else:
-                    movimentos.append(pos)  # Casa vazia
-
-        return movimentos
+                    movimentos_possíveis.append((nova_linha, nova_coluna))  # Casa vazia
+        
+        return movimentos_possíveis
