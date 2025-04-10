b466f05d3053fc303a05c0ff875e76875b347658
Flavitche <fves@cin.ufpe.br>
2025-04-10 17:23:47 -0300

    Rei atualizado para xeque-mate

--- rei.py
+++ rei.py
@@ -1,15 +1,11 @@
 from base_peça import Base
 
 class Rei(Base):
-
-    #O método construtor é herdado da classe Base.
-
     tipo = 'rei'
     movida = False
-    em_xeque = False #No início do jogo, ambos dos reis não estão em xeque.
+    em_xeque = False  # No início do jogo, o rei não está em xeque
 
     def movimentos_possíveis(self, info_peças):
-        # Aqui você deve implementar a lógica para determinar os movimentos possíveis do rei
         movimentos_possíveis = []
         linha, coluna = self.casa
 
@@ -17,7 +13,7 @@ class Rei(Base):
         for d_linha in [-1, 0, 1]:
             for d_coluna in [-1, 0, 1]:
                 if d_linha == 0 and d_coluna == 0:
-                    continue  # Ignora a posição atual
+                    continue  # Posição atual
                 nova_casa = (linha + d_linha, coluna + d_coluna)
                 if self.casa_valida(nova_casa, info_peças):
                     movimentos_possíveis.append(nova_casa)
@@ -25,9 +21,9 @@ class Rei(Base):
         return movimentos_possíveis
 
     def casa_valida(self, nova_casa, info_peças):
-        # Verifica se a nova casa está dentro dos limites do tabuleiro
+        # Dentro dos limites do tabuleiro
         if 0 <= nova_casa[0] < 8 and 0 <= nova_casa[1] < 8:
-            # Verifica se a casa não está ocupada por uma peça da mesma cor
+            # Não pode ser ocupada por uma peça da mesma cor
             for grupo_cor in info_peças:
                 for peça in info_peças[grupo_cor]:
                     if peça.casa == nova_casa and peça.cor == self.cor:
